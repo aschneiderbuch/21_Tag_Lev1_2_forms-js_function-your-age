47c41529e2504a23a09945d73d651ddcc36d3de7
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-11 14:49:08 +0100

    testingPhase

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,4 +14,21 @@ in Html wird das Alter geschrieben
 
  */
 
+// Varibeln über die ids holen
+let inputGeburtsjahr = document.getElementById("inputGeburtsjahr");
+let outputGeburtsjahr = document.getElementById("outputGeburtsjahr");
+let buttonSubmit = document.getElementById("buttonSubmit");
 
+// Event holen       und in Variable packen
+buttonSubmit.addEventListener("click", rechnenAlter);
+
+
+// function rechnenAlter
+function rechnenAlter(){
+    let ergebnis = ( 2022 - inputGeburtsjahr.value );
+
+    console.log(ergebnis);
+
+    outputGeburtsjahr.innerHTML = ergebnis;
+
+}
