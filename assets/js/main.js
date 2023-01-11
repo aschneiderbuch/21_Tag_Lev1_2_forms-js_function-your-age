1e9e01178c1c2150d8f01d5e379e8aa79e83fc04
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-11 14:33:47 +0100

    Frage

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/*  Aufgabe
+
+input Geburtsjahr
+        id type:age    +    let age
+
+
+Button rechnet
+        function rechnenAlter 
+                     age - aktuelle JahresDatum
+
+        let output    + innerHTML 
+
+in Html wird das Alter geschrieben
+
+ */
+
+
